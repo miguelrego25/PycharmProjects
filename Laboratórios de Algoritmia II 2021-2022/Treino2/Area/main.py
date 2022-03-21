5c3318d812b0693cc0e5da26d16781a26a41fe0b
Miguel Rego <miguel.m.rego@gmail.com>
2022-03-21 19:54:48 +0000

    s

---
+++ Laboratórios de Algoritmia II 2021-2022/Treino2/Area/main.py
@@ -0,0 +1,57 @@
+'''
+
+Implemente uma função que calcula a área de um mapa que é acessível por
+um robot a partir de um determinado ponto.
+O mapa é quadrado e representado por uma lista de strings, onde um '.' representa
+um espaço vazio e um '*' um obstáculo.
+O ponto inicial consistirá nas coordenadas horizontal e vertical, medidas a
+partir do canto superior esquerdo.
+O robot só consegue movimentar-se na horizontal ou na vertical.
+
+'''
+
+#p sao as coordenadas de origem
+#mapa sao as listas de strings que conteem os pontos e os asteriscos
+
+def area(p,mapa):
+    vis=[]
+    print(area1(p,mapa,vis))
+    return len (vis)
+
+def area1(p,mapa,vis):
+    x = p[0]
+    y = p[1]
+    vis.append(p)
+    if x+1==len(mapa[1]) or y+1==len(mapa[1]):
+        return 0
+
+    else:
+        if mapa[x+1][y] == '.' and (x+1,y) not in vis:
+            area1((x+1,y),mapa,vis)
+        if mapa[x][y+1] == '.' and (x,y+1) not in vis:
+            area1((x, y+1), mapa, vis)
+        if mapa[x-1][y] == '.' and (x-1,y) not in vis:
+            area1((x-1, y), mapa, vis)
+        if mapa[x][y-1] == '.' and (x,y-1) not in vis:
+            area1((x, y-1), mapa, vis)
+
+
+
+
+
+
+
+def main():
+    print("<h3>area</h3>")
+    mapa = ["..*..",
+            ".*.*.",
+            "*....",
+            ".*.*.",
+            "..*.."]
+    print("in:",(3,2))
+    print('\n'.join(mapa))
+    print("out:",area((3,2),mapa))
+
+if __name__ == '__main__':
+    main()
+
