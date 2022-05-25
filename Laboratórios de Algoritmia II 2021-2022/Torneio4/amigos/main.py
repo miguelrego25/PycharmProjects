bef9c661293ab7852fa447164e83287a9a86cffe
Miguel Rego <miguel.m.rego@gmail.com>
2022-05-25 12:47:50 +0100

    first amigos

--- Laboratórios de Algoritmia II 2021-2022/Torneio4/amigos/main.py
+++ Laboratórios de Algoritmia II 2021-2022/Torneio4/amigos/main.py
@@ -24,20 +24,97 @@ def amigos(conhecidos):
     list = sorted(list, reverse=True)
     print(list)
     return list[0]
+
+def first(h):
+
+
+def next(h, c):
+
+def valid(h, c):
+
+
+def main(conhecidos):
+
+    c = first(conhecidos)
+    while (c != None):
+        if valid(conhecidos,c):
+            return c
+        c = next(conhecidos,c)
+
+
+
+
+def checknames(nome, conhecidos,verif):
+    count = 0
+    for primeiro, segundo in conhecidos:
+        if primeiro == segundo:
+            count += 0
+        elif primeiro == nome or segundo == nome and primeiro not in verif or segundo not in verif:
+            if primeiro == nome:
+                verif.append(primeiro)
+                count += 1
+            elif segundo == nome:
+                verif.append(segundo)
+                count += 1
+    return count
+
+
+def amigos(conhecido):
+    conhecidos = list(conhecido)
+    dic = {}
+    nameschecked = []
+    maxi = 0
+
+    for primeiro,segundo in conhecidos:
+
+        if primeiro == segundo:
+            maxi += 0
+        elif primeiro not in nameschecked:
+            s = checknames(primeiro,conhecidos[1:],[])
+            s += 1
+            if maxi < s:
+                maxi = s
+            nameschecked.append(primeiro)
+        if segundo not in nameschecked:
+            s=checknames(segundo,conhecidos[1:],[])
+            s += 1
+            if maxi < s:
+                maxi = s
+            nameschecked.append(segundo)
+    return maxi
 '''
 
+def searchconhecidos(primeiro,conhecidos):
+    listamigos = []
+    for fstnome,scdnome in conhecidos:
+        if fstnome != scdnome:
+            if primeiro == scdnome and fstnome not in listamigos:
+                listamigos.append(fstnome)
+            if primeiro == fstnome and scdnome not in listamigos:
+                listamigos.append(scdnome)
+    return listamigos
 
-def main(h):
- c = first(h)
- while (c != None):
- if valid(h,c):
- return c
- c = next(h,c)
+def amigos(s):
+    conhecidos = list(s)
+    dic = {}
+    max = 0
+
+    for fstnome,scdnome in conhecidos:
+        if fstnome != scdnome:
+            if fstnome not in dic.keys():
+                dic[fstnome] = searchconhecidos(fstnome, conhecidos)
+            if scdnome not in dic.keys():
+                dic[scdnome] = searchconhecidos(scdnome, conhecidos)
+
+    print(dic)
+    newlist = sorted(dic.items(), key=lambda x: len(x[1]),reverse=True)
+    return len(newlist[0][1])
+
 
 
 def main():
     print("<h3>amigos</h3>")
-    conhecidos = {('pedro','maria'),('jose','francisca'),('pedro','pedro')}
+    conhecidos = conhecidos = {('pedro', 'maria'), ('pedro', 'jose'), ('pedro', 'manuel'), ('maria', 'jose'), ('maria', 'francisca'), ('jose', 'francisca'), ('francisca', 'manuel')}
     print("in:", conhecidos)
     print("out:", amigos(conhecidos))
 
